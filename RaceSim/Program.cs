cd2b37262f81f579f528409150616c27ef7c8ede
Student-Nathan <s1169229@student.windesheim.nl>
2022-09-14 17:25:35 +0200

    small fixes

--- RaceSim/Program.cs
+++ RaceSim/Program.cs
@@ -1,5 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-using Controller;
+﻿using Controller;
 using Model;
 internal class Program {
     private static void Main(string[] args) {
