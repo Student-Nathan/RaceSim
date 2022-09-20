01ae4b8061f570892f77a62d2001d0984281ae14
Student-Nathan <s1169229@student.windesheim.nl>
2022-09-20 10:13:00 +0200

    Begin 4-2

---
+++ RaceSim/visual.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+using Controller;
+
+namespace RaceSim {
+    public static class visual {
+        
+        public static void initialize() {
+            
+        }
+
+        #region graphics
+        private static string[] _finishHorizontal = { "----", "  # ", "  # ", "----" };
+        private static string[] _finishVertical = { };
+        private static string[] _straightHorizontal = { };
+        private static string[] _straightVertical = { };
+        private static string[] _leftHorizontal = { };
+        private static string[] _leftVertical = { };
+        private static string[] _rightHorizontal = { };
+        private static string[] _rigthVertical= { };
+        private static string[] _startGrid = { };
+
+        #endregion
+    }
+
+}
